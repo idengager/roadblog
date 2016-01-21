3ffcbd71b5050c6433abd0329a8b6f65741a759d
Kasia Jarmołkowicz <kasia.jarmolkowicz@gmail.com>
2016-01-21 12:51:56 +0100

    Send action to controller with args

--- app/components/display-color.js
+++ app/components/display-color.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
     return "Color " + this.get('color.name');
   }.property("color.name"),
 
-  doubleClick(){
-    alert("Your color is " + this.color.name);
+  click: function() {
+    this.sendAction("actionname", this.get('fullName'));
   }
 });
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,5 +4,11 @@ export default Ember.Controller.extend({
   colorCount: Ember.computed("model.[]", function() {
     var colors = this.get('model');
     return colors.get('length');
-  })
+  }),
+
+  actions: {
+    sayHello(color) {
+      alert("Hiiii! " + color);
+    }
+  }
 });
