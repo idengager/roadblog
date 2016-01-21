3dac92d3c8ea790e7d9b220b6ffc49e34b539809
Kasia Jarmołkowicz <kasia.jarmolkowicz@gmail.com>
2016-01-21 12:30:59 +0100

    Action on component double click

--- app/components/display-color.js
+++ app/components/display-color.js
@@ -4,4 +4,8 @@ export default Ember.Component.extend({
   fullName: function() {
     return "Color " + this.get('color.name');
   }.property("color.name"),
+
+  doubleClick(){
+    alert("Your color is " + this.color.name);
+  }
 });
