d1c8eab6f5ff7001a1a7df80d1d339716ca448f6
Kasia Jarmołkowicz <kasia.jarmolkowicz@gmail.com>
2016-01-28 13:06:18 +0100

    Fix posts.edit
    Forgot to inject session service object.

--- app/routes/posts/edit.js
+++ app/routes/posts/edit.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import AuthenticatedRoute from './authenticated-route';
 
 export default Roadblog.AuthenticatedRoute.extend({
+  session: Ember.inject.service('session'),
+
   actions: {
     save: function(model) {
       model.save().then(() => this.transitionTo('posts.index'));
