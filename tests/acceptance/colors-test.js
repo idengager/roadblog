7a4c298862a78f075fc8e6aa25201616a5e6396d
Kasia Jarmołkowicz <kasia.jarmolkowicz@gmail.com>
2016-01-29 11:43:22 +0100

    WIP Acceptance tests

---
+++ tests/acceptance/colors-test.js
@@ -0,0 +1,10 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'roadblog/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | colors');
+
+test('visiting /index', function(assert) {
+  visit('/');
+
+  andThen(() => assert.equal(currentURL(), '/'));
+});
---
+++ tests/acceptance/sign-in-test.js
@@ -0,0 +1,28 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'roadblog/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | sign-in');
+
+test('visiting /sing-in', function(assert) {
+  visit('/sign-in');
+  andThen(() => assert.equal(currentURL(), '/sign-in'));
+});
+
+test('valid credentials', function(assert) {
+  visit('/sign-in');
+  andThen(() => assert.equal(currentURL(), '/sign-in'));
+
+  fillIn('input#username', 'admin');
+  fillIn('input#password', 'password');
+  click('button:submit');
+  andThen(() => assert.equal(currentURL(), '/posts'));
+});
+
+test('invalid credentials', function(assert) {
+  visit('/sign-in');
+
+  fillIn('input#username', 'test');
+  fillIn('input#password', 'test');
+  click('button:submit');
+  andThen(() => assert.equal(currentURL(), '/sign-in'));
+});
