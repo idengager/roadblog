733bde9d38fe4ccd9f78486943a280a907e7ae7a
Kasia Jarmołkowicz <kasia.jarmolkowicz@gmail.com>
2016-02-29 16:28:21 +0100

    Fix colors acceptance test

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -3,4 +3,13 @@ export default function() {}
 export function testConfig() {
   this.get('/posts');
   this.get('/posts/non-existent', {}, 404);
+
+  var colors = {
+    'colors': [
+        {'name': 'red'},
+        {'name': 'white'},
+        {'name': 'blue'}
+    ]
+  };
+  this.get('colors.json', colors, 200)
 }
--- tests/acceptance/colors-test.js
+++ tests/acceptance/colors-test.js
@@ -4,10 +4,9 @@ import moduleForAcceptance from 'roadblog/tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | colors');
 
 test('visiting /index', function(assert) {
-  // TODO
-  // add data via mirage's fixtures
-  // this test currently times out/passes randomly
   visit('/');
 
-  andThen(() => assert.equal(currentURL(), '/'));
+  andThen(() => {
+    assert.equal(currentURL(), '/');
+  });
 });
