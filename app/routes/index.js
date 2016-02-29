56cf62f8d994663ffa6869fe72ff608fd7ac17f0
Kasia Jarmołkowicz <kasia.jarmolkowicz@gmail.com>
2016-02-29 14:33:27 +0100

    Clean-up

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,11 @@
-import Ember from "ember";
+import Ember from 'ember';
 
 var url = 'colors.json';
 
 export default Ember.Route.extend({
   model: function() {
-    return $.getJSON(url).then(function(data) {
-      return $.map(data.colors, function(e) { return Ember.Object.create(e); });
+    return Ember.$.getJSON(url).then((data) => {
+      return Ember.$.map(data.colors, function(e) { return Ember.Object.create(e); });
     });
   }
 });
